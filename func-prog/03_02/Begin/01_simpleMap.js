019258c210e5db5a42e9fff70b6a6bfbdb0bda32
chullman <christopherhullman@gmail.com>
2018-05-26 11:49:07 +1000

    exercise completion

--- func-prog/03_02/Begin/01_simpleMap.js
+++ func-prog/03_02/Begin/01_simpleMap.js
@@ -1,6 +1,8 @@
 // Learning Functional Programming with Javascript
 // Chapter 03, Video 02, Exercise 01
 
+var _ = require("lodash")
+
 var numbers = [ 1, 2, 3, 4, 5 ]
 var numbersCubed = [ ]
 
@@ -8,3 +10,9 @@ for(var i = 0; i < numbers.length; i++) {
   var element = numbers[i]
   numbersCubed.push(element * element * element)
 }
+
+numbersCubed = _.map(numbers, function(element) {
+  return element * element * element
+})
+
+console.log(numbersCubed)
