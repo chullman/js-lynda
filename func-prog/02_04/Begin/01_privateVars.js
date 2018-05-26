d8bd88d051d736625ba0af601931282de4356369
chullman <christopherhullman@gmail.com>
2018-05-26 11:49:29 +1000

    exercise completion

--- func-prog/02_04/Begin/01_privateVars.js
+++ func-prog/02_04/Begin/01_privateVars.js
@@ -1,23 +1,38 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 04, Exercise 01
 
-const myCounter = {
-  count: 0,
-  increment: function() {
-    this.count += 1
-  },
-  currentValue: function() {
-    return this.count
+// OLD
+// const myCounter = {
+//   count: 0,
+//   increment: function() {
+//     this.count += 1
+//   },
+//   currentValue: function() {
+//     return this.count
+//   }
+// }
+
+
+// BETTER (prevents count var from being public scope)
+function createCounter() {
+  var count = 0
+  return {
+    increment: function() {
+      count += 1
+    },
+    currentValue: function() {
+      return count
+    }
   }
 }
 
+var myCounter = createCounter();
+
 console.log(myCounter.currentValue())
 
 myCounter.increment()
 myCounter.increment()
 
 console.log(myCounter.currentValue())
-
-myCounter.count = 999 // uh oh!
 
 console.log(myCounter.currentValue())
