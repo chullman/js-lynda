44824cb389188ed77bfe8534215be49ee93e3281
chullman <christopherhullman@gmail.com>
2018-05-27 11:08:45 +1000

    added packages

--- func-prog/03_03/Begin/01_simpleFilter.js
+++ func-prog/03_03/Begin/01_simpleFilter.js
@@ -1,12 +1,24 @@
+// // Learning Functional Programming with Javascript
+// // Chapter 03, Video 03, Exercise 01
+
+// var numbers = [ 1, 2, 3, 4, 5 ]
+// var evensFromNumbers = [ ]
+
+// for (var i = 0; i < numbers.length; i++) {
+//   var element = numbers[i]
+//   if (element % 2 === 0) {
+//     evensFromNumbers.push(element)
+//   }
+// }
+
+
 // Learning Functional Programming with Javascript
-// Chapter 03, Video 03, Exercise 01
+// Chapter 03, Video 03, Exercise 01 **SOLUTION**
+var _ = require("lodash")
 
 var numbers = [ 1, 2, 3, 4, 5 ]
-var evensFromNumbers = [ ]
+var evensFromNumbers = _.filter(numbers, function(element) {
+  return element % 2 === 0
+})
 
-for (var i = 0; i < numbers.length; i++) {
-  var element = numbers[i]
-  if (element % 2 === 0) {
-    evensFromNumbers.push(element)
-  }
-}
+console.log(evensFromNumbers)
---
+++ learn-node/demo.js
@@ -0,0 +1,18 @@
+// REQUIRING LODASH
+
+// var _ = require('lodash')
+
+// console.log(_.random(1,100))
+
+
+// READING FROM FS
+
+// var fs = require('fs')
+// var data = require('./data.json')
+
+// console.log(data.name)
+
+// fs.readFile('./data.json', 'utf-8', (err, data) => {
+//   var data = JSON.parse(data)
+//   console.log(data.name)
+// })
